12b4a5a5bb4275bec40983a11314c9418d785f70
Ananya Singhal <48484581+reachananya@users.noreply.github.com>
2021-11-25 02:44:28 +0530

    Time: 1 ms (32.06%), Space: 41.4 MB (26.51%) - LeetHub

--- flipping-an-image/flipping-an-image.java
+++ flipping-an-image/flipping-an-image.java
@@ -6,22 +6,16 @@ class Solution {
         //last element in the row -> [0][3] = [i][k]
         for(int i = 0 ; i<image.length; i++){
             int j = 0;
-            int k = image.length- 1;
+            int k = image[i].length- 1;
             while(j<k){
                 int temp = image[i][j];
-                image[i][j] = image[i][k];
-                image[i][k] = temp;
-                j++;
-                k--;
+                image[i][j++] = image[i][k];
+                image[i][k--] = temp;
             }
             
             //converting 0 -> 1 & 1 -> 0
             for(int a=0; a<image[i].length; a++){
-                if(image[i][a] == 0){
-                    image[i][a] = 1;
-                }else{
-                    image[i][a] = 0;
-                }
+                image[i][a] = image[i][a] == 1 ? 0 : 1;
             }
              
         }
