5f539ea0d67010903fac6e8448b94bb9e61bbd32
Ananya Singhal <48484581+reachananya@users.noreply.github.com>
2022-07-11 02:21:38 +0800

    Time: 925 ms (18.45%), Space: 507.2 MB (5.82%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -2,17 +2,17 @@ class Solution {
     public boolean isPalindrome(String s) {
         String st = "";
         
-        for (char c : s.toCharArray()){
-            if( Character.isLetter(c) || Character.isDigit(c)){
-                st = st+c;
+        for(char c: s.toCharArray()){
+            if(Character.isDigit(c) || Character.isLetter(c)){
+                st+=c;
             }
-            
         }
         st = st.toLowerCase();
+        
         int start = 0;
         int end = st.length()-1;
         
-        while(start <= end){
+        while(start<=end){
             if(st.charAt(start) != st.charAt(end)){
                 return false;
             }
