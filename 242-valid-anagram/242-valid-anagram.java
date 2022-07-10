2f437711460407b06cabe06873fb22e52e8f7043
Ananya Singhal <48484581+reachananya@users.noreply.github.com>
2022-07-11 03:11:57 +0800

    Time: 5 ms (75.13%), Space: 43.4 MB (74.01%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,21 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
-        Map<Character, Integer> mp = new HashMap<>();
-        for(int i = 0; i<s.length(); i++){
-            //tells that teh character is not their in the map 
-            if(mp.get(s.charAt(i)) == null){
-                mp.put(s.charAt(i), 1);
-            }else{
-                mp.put(s.charAt(i), mp.get(s.charAt(i)) + 1);
-            }
-        }
-        
-        //now we will compare the string t with teh map mp
-        for(int j = 0; j<t.length(); j++){
-            if(mp.get(t.charAt(j)) == null || mp.get(t.charAt(j)) == 0) return false;
-            mp.put(t.charAt(j), mp.get(t.charAt(j)) -1);
-        }
-        return true;
+	var sLength = s.length();
+	if (sLength != t.length())
+		return false;
+
+	var freq = new int[26];
+	for (var i = 0; i < sLength; i++)
+		freq[s.charAt(i) - 'a']++;
+
+	for (var i = 0; i < sLength; i++) 
+		if (freq[t.charAt(i) - 'a']-- == 0)
+			return false;
+	return true;  
     }
 }
