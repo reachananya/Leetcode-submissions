3742f8275d7af2e0f719688b2694a99c86a8a925
Ananya Singhal <48484581+reachananya@users.noreply.github.com>
2022-10-17 23:09:48 +0530

    Time: 1 ms (63.96%), Space: 44 MB (37.70%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -13,32 +13,24 @@
  *     }
  * }
  */
+
+//left subtree should be less than the node and right subtree should be greater 
+
 class Solution {
     public boolean isValidBST(TreeNode root) {
         
-        return validate(root, null, null);
+        if(root == null) return true;
         
+        return validate(root, null, null);
     }
     
-    //Recursive function
     public boolean validate(TreeNode root, Integer max, Integer min){
-        
-        // step 1 - 
-        // define the base cases 
-        
-        //if we have reached the root which is null that means all conditions were followed and we can return true
-        
         if(root == null){
             return true;
-        }else if (max != null && root.val >= max || min != null && root.val <= min){
+        }else if(max != null && root.val >= max || min != null && root.val <= min){
             return false;
-            
         }else{
             return validate(root.left, root.val, min) && validate(root.right, max, root.val);
         }
-        
-        //step 2 - 
-        // define the recursive calls
-        
     }
 }
