4f799a3a4a8748b59389bac5cf3a6d540df1708e
Ananya Singhal <48484581+reachananya@users.noreply.github.com>
2022-10-18 03:45:14 +0530

    Time: 36 ms (21.05%), Space: 54.5 MB (15.28%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int res = 0;
+        
+        for(int i = 0; i<n; i++){
+            boolean[] visited = new boolean[256];
+            
+            for(int j = i; j<n; j++){
+                if(visited[s.charAt(j)] == true){
+                    break;
+                }else{
+                    res = Math.max(res, j - i + 1);
+                    visited[s.charAt(j)] = true;
+                }
+            }
+            visited[s.charAt(i)] = false;
+        }
+        return res;
+    }
+    
+}
+
+
+
+/*
+//initializing the return var
+        int maxLength = 0;
+        
+        for(int i = 0; i<s.length(); i++){
+            for(int j  = i; j<s.length(); j++){
+                if(areUnique(s, i, j) == true){
+                    maxLength = Math.max(maxLength, j-i+1);
+                }
+            }
+        }
+        return maxLength;  
+    }
+    public boolean areUnique(String s, int i, int j){
+        int[] visited = new int[256]; 
+        
+        for(int k = i; k<s.length(); k++){
+            if(visited.charAt(s.charAt(k)) == true){
+                //that means its already visited hence, we will make it false
+                return false;
+            }else{
+                visited[s.charAt(k)] = true;
+            }
+            
+        }
+        return true;
+*/
