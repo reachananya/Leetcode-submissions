79938c184a2554366d4fabb20c4c98671eaa3ee6
Ananya Singhal <48484581+reachananya@users.noreply.github.com>
2022-10-17 15:15:12 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (95.33%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
@@ -23,6 +23,8 @@ class Solution {
         }
         
         root1.val += root2.val;
+        
+        //recursive calls
         root1.left = mergeTrees(root1.left, root2.left);
         root1.right = mergeTrees(root1.right, root2.right);
         
