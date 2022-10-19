3e9dc6a80f431716d6dcaf172b4c6e87fb0b815d
Ananya Singhal <48484581+reachananya@users.noreply.github.com>
2022-10-19 19:07:28 +0530

    Time: 10 ms (40.25%), Space: 44.5 MB (64.36%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,6 +1,24 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        int[] timeStamp = new int[1001];
+        int in_car = 0;
+        int[] increase = new int[1001];
+        for (int i = 0; i < trips.length; i ++) { // find out the number of the **net increase** passengers for each stop
+            increase[trips[i][1]] += trips[i][0];
+            increase[trips[i][2]] -= trips[i][0];
+        }
+        for (int i = 0; i <= 1000; i ++) { // from start to end, for each stop we calculate the number of passengers in the car.
+            in_car += increase[i];
+            if (in_car > capacity) return false; // once the number of passengers in the car exceeds the capacity
+        }
+        return true;
+        
+        
+    }
+}
+
+
+/*
+int[] timeStamp = new int[1001];
         
         for(int[] t : trips){
             timeStamp[t[1]] += t[0];
@@ -14,7 +32,5 @@ class Solution {
             }
         }
         return true;
-        
-        
-    }
-}
+
+*/
